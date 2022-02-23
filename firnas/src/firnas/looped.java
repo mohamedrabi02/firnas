bf23a87e7a5f77dd7919fa3c903d834be1c1f9ae
HP <HP@192.168.1.37>
2022-02-23 16:38:42 +0530

    first commit

---
+++ firnas/src/firnas/looped.java
@@ -0,0 +1,14 @@
+package firnas;
+
+public class looped {
+	public static void main(String[] args)
+	{
+		int i=10;
+		for(int a=0;a<i;a++) 
+		{
+			System.out.println("legends never die::");
+		}
+	}
+	
+
+}
