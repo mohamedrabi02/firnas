89b0b7ba02ca12c31fbf155c2f28e07833af19a5
HP <HP@LAPTOP-TFUKMQC6>
2022-03-14 16:03:26 +0530

    golden words

---
+++ firnas/src/firnas/Addition.java
@@ -0,0 +1,15 @@
+package firnas;
+
+public class Addition {
+	public static void main(String[] args) {
+		System.out.println("enter two numbers:");
+		int first=100;
+		int second=200;
+		System.out.println(first+" "+second);
+		int sum=first+second;
+		System.out.println("the sum is="+sum);
+		
+		
+	}
+
+}
---
+++ firnas/src/firnas/vowel.java
@@ -0,0 +1,13 @@
+package firnas;
+
+public class vowel {
+	public static void main(String[] args) {
+		char ch='i';
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+			System.out.println("the char"+ch+"is a vowel");
+		}
+		else
+			System.out.println("the char i is not a vowel");
+	}
+
+}
