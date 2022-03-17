8b9a0d3adb6f99100b7a90d3f666b6a6a584fec3
HP <HP@LAPTOP-TFUKMQC6>
2022-03-17 07:25:17 +0530

    add

--- firnas/src/firnas/Addition.java
+++ firnas/src/firnas/Addition.java
@@ -1,12 +1,15 @@
 package firnas;
+import java.util.Scanner;
 
 public class Addition {
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
 		System.out.println("enter two numbers:");
-		int first=100;
-		int second=200;
+		int first=sc.nextInt();
+		int second=sc.nextInt();
 		System.out.println(first+" "+second);
 		int sum=first+second;
+		sc.close();
 		System.out.println("the sum is="+sum);
 		
 		
