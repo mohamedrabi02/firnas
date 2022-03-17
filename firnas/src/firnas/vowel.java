5da0b497898c1449d5a5fb520758aff5cfb7d6b4
HP <HP@LAPTOP-TFUKMQC6>
2022-03-17 07:19:48 +0530

    vowel

--- firnas/src/firnas/vowel.java
+++ firnas/src/firnas/vowel.java
@@ -1,7 +1,15 @@
+
+
 package firnas;
+import java.util.Scanner;
 
 public class vowel {
 	public static void main(String[] args) {
+		System.out.println("enter a name:");
+		Scanner s=new Scanner(System.in);
+		String c= s.next();
+		s.close();
+		System.out.println("the entered name is:"+c);
 		char ch='i';
 		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
 			System.out.println("the char"+ch+"is a vowel");
