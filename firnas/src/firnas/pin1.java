1efc5aafb7c0e81d2dfb8a6bcaa2dd74a01179f3
HP <HP@LAPTOP-TFUKMQC6>
2022-03-19 21:48:00 +0530

    dam

---
+++ firnas/src/firnas/pin1.java
@@ -0,0 +1,29 @@
+package firnas;
+
+public class pin1 {
+
+	public static void main(String[] args) {
+		e b=new e();
+		s a=new s();
+		a.rabi(b);
+	}
+
+}
+class s
+{
+  	public void rabi(s1 f) {
+  		System.out.println(f.sed());
+  	}
+}
+abstract class s1
+{
+  abstract void sed();	
+}
+class d extends s1{
+	void sed() 
+	{System.out.println("class d called");}
+}
+class e extends s1{
+	void sed() 
+	{System.out.println("class e called");}
+}
