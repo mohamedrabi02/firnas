78d93fc094d4a51de9730a5b94ecc37d66ac0359
HP <HP@LAPTOP-TFUKMQC6>
2022-03-21 17:26:37 +0530

    static block and object

---
+++ firnas/src/firnas/ststicdemo1.java
@@ -0,0 +1,31 @@
+package firnas;
+
+public class ststicdemo1 {
+
+	public static void main(String[] args) {
+		room r=new room();
+		room r2=new room();
+		//static() {
+			
+		//}
+
+	}
+	static {
+		System.out.println("static block is called in class ststicdemo1.....");
+	}
+
+}
+class room{
+	table t=new table();
+	static fan f=new fan();
+}
+class table{
+	public table() {
+		System.out.println("wooden table in room");
+	}
+}
+class fan{
+	public fan() {
+		System.out.println("crompton fan in room..");
+	}
+}
